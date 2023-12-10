ab9d4c6a11bdbb0b35809bf3f26674fe344be6ec
bdemir97 <idemir@sabanciuniv.edu>
2023-12-10 23:49:41 +0100

    Update indicators.py

--- functions/indicators/indicators.py
+++ functions/indicators/indicators.py
@@ -23,7 +23,7 @@ def heikin_yesterday(symbol, binance_spot_api, CANDLE_LENGTH):
     return candle['close'] - candle['open']
 
 def moving_averages(symbol, binance_spot_api, CANDLE_LENGTH, SHORT_MA, LONG_MA):
-    return calculate_ma(symbol, SHORT_MA, binance_spot_api), calculate_ma(symbol,LONG_MA, binance_spot_api, CANDLE_LENGTH)
+    return calculate_ma(symbol, SHORT_MA, binance_spot_api, CANDLE_LENGTH), calculate_ma(symbol, LONG_MA, binance_spot_api, CANDLE_LENGTH)
 
 def dema(symbol, binance_spot_api, CANDLE_LENGTH, SHORT_DEMA, LONG_DEMA):
     return calculate_dema(symbol, binance_spot_api, SHORT_DEMA, CANDLE_LENGTH), calculate_dema(symbol, binance_spot_api, LONG_DEMA, CANDLE_LENGTH)

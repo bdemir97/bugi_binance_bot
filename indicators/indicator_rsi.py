45765c917934b27ec4f9b7179bdb1e845668c0af
bdemir97 <idemir@sabanciuniv.edu>
2023-12-11 14:18:58 +0100

    rsi with smoothed average

--- indicators/indicator_rsi.py
+++ indicators/indicator_rsi.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 
 def calculate_rsi(symbol, num_days, binance_spot_api, candle_length):
     end_time = datetime.now()
-    start_time = datetime.now() - timedelta(days=num_days)
+    start_time = datetime.now() - timedelta(days=num_days+1)
 
     klines = binance_spot_api.get_historical_klines(
         symbol=symbol,
@@ -11,17 +11,24 @@ def calculate_rsi(symbol, num_days, binance_spot_api, candle_length):
         end_str=str(int(end_time.timestamp() * 1000)))
     
     closes = [float(kline[4]) for kline in klines]
+
     price_changes = [closes[i] - closes[i - 1] for i in range(1, len(closes))]
+    
     gains = [change if change > 0 else 0 for change in price_changes]
     losses = [-change if change < 0 else 0 for change in price_changes]
 
-    avg_gain = sum(gains) / num_days
-    avg_loss = sum(losses) / num_days
+    avg_gain = calculate_average(gains, num_days)
+    avg_loss = calculate_average(losses, num_days)
 
     rs = avg_gain / avg_loss if avg_loss != 0 else 0
     rsi = 100 - (100 / (1 + rs))
     return rsi
 
+def calculate_average(data, num_days):
+    total = 0
+    for i in range(len(data) - num_days, len(data)):
+        total += data[i]
+    return total / num_days
 
 
 
